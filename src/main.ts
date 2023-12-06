cf012ec34091f926d7fb75696047677ce0c71c14
mariobros123413 <j.mario18@hotmail.es>
2023-12-06 09:37:31 -0400

    cors2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { join } from 'path'; // Importa la función 'join' de Node.js para trabajar con rutas
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,10 @@ async function bootstrap() {
   app.use(bodyParser.json());
   app.use(cors());
   app.enableCors();
+  app.use(cors({
+    origin: '*',
+    credentials: true,
+  }));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/images', express.static(join(__dirname, '..', 'images')));
   app.use('/videos', express.static(join(__dirname, '..', 'videos')));
